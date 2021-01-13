70b7c1378766d3a6b9e7a28e2518906938583e30
florishi <flroisihi12@gmail.com>
2021-01-13 22:42:41 +0000

    added routes for history and html routes

---
+++ routes/html.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+// route for home page
+router.get('/', (req, res) => {
+  res.send('homepage: hello team!');
+});
+
+// route for login page - need to look into Google Auth
+router.get('/login', (req, res) => {
+  res.send('login page');
+});
+
+// route for page not found - need to set 404 header
+router.get('*', (req, res) => {
+  res.send('page not found 404');
+});
+
+module.exports = router;
