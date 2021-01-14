c45624012a2b67397d64ee958efdf644a58b1c19
florishi <flroisihi12@gmail.com>
2021-01-14 20:35:40 +0000

    tweaked routes

--- routes/history.js
+++ routes/history.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // route to send user history logs
-router.get('/history', (req, res) => {
+router.get('/', (req, res) => {
   res.render('history');
 });
 
