a83bbffa8b9e5b4de6afdc7363ce04e0c2f223b7
florishi <flroisihi12@gmail.com>
2021-01-15 23:54:00 +0000

    added redirects to html route and nanoid

--- routes/html.js
+++ routes/html.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { nanoid } = require('nanoid');
 
 // route for home page
 router.get('/', (req, res) => {
+  //console.log(req.session);
   res.render('index');
 });
 
-// route for login page - need to take express-session id
+// route for login page
 router.get('/login', (req, res) => {
   res.render('login');
 });
 
-// route for clearing session and redirecting to homepage
-router.get('/logout', (req, res) => {
-  res.redirect('/');
+// route for login page to accept user login form values
+router.post('/login', (req, res) => {
+  const { name, email } = req.body;
+  console.log(`email is: ${email}`);
+  console.log(`name: ${name}`);
+  const id = nanoid();
+  //-------------CREATE SEQUELIZE QUERY HERE TO SAVE TO DB------------//
+  return res.send({redirect: `/dashboard/?=${id}`});
 });
 
 // route for page not found
 router.get('*', (req, res) => {
-  res.send('sorry, page not found').status(404);
+  res.status(404).send('Sorry, we cannot find that page!');
 });
 
 module.exports = router;
