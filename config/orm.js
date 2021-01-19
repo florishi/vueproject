b3696aad10172417c3a00a60e017c25c5dcc112d
florishi <flroisihi12@gmail.com>
2021-01-19 20:31:33 +0000

    added the history query, changed db,orm and models

--- config/orm.js
+++ config/orm.js
@@ -1,28 +1,28 @@
 const {connection,DataTypes} = require('./db');
 
 const Users = connection.define('User', {
-  id      :{type: DataTypes.STRING(20), allowNull: false, primaryKey: true, autoIncrement: true },
-  Email   :{type: DataTypes.STRING(50)},
-  userName:{type: DataTypes.STRING(50)}
+  id       :{type: DataTypes.STRING(22), allowNull: false, primaryKey: true, autoIncrement: true },
+  Email    :{type: DataTypes.STRING(50)},
+  userName :{type: DataTypes.STRING(50)}
 },{
   timestamps : false,
 });
 
 const Stress = connection.define('Stress', {
   id           :{type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
-  usersID      :{type: DataTypes.INTEGER },
+  usersID      :{type: DataTypes.STRING(22)},
   moods        :{type: DataTypes.STRING(10)},
-  hoursSleep  :{type:DataTypes.INTEGER },
-  minsExercise:{type:DataTypes.INTEGER},
-  coffeeUnits :{type:DataTypes.INTEGER},
-  inputDate   :{type:DataTypes.DATE}
+  hoursSleep   :{type:DataTypes.INTEGER },
+  minsExercise :{type:DataTypes.INTEGER},
+  coffeeUnits  :{type:DataTypes.INTEGER},
+  inputDate    :{type:DataTypes.DATE}
 },{
   timestamps : false,
 });
 
 const Health = connection.define('health', {
   id           :{type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
-  usersID      :{type: DataTypes.INTEGER },
+  usersID      :{type: DataTypes.STRING(22)},
   waterUnits   :{type:DataTypes.INTEGER },
   alchoolUnits :{type:DataTypes.INTEGER},
   steps        :{type:DataTypes.INTEGER},
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
-Insert into Users(Email,userName) VALUES ("g@gmail.com","guga");
-Insert into Stresses(usersID,moods,hoursSleep,minsExercise,coffeeUnits,inputDate) 
-VALUES (1,"happy",8,30,0,"2020-01-14");
-Insert into health(usersID,weight,waterUnits,alchoolUnits,steps,calories,inputDate) 
-VALUES (1,73,8,0,1000,3000,"2020-01-14");
-/*Select * from Users;
-Select * from stress;*/
-Select * from stresses;
+Insert into Users(id,Email,userName) VALUES ("2323e2wdwqdw123","g@gmail.com","Guga");
+Insert into Stresses(usersID,moods,hours_sleep,mins_exercise,coffee_units,input_date) 
+VALUES ("2323e2wdwqdw123","happy",8,30,0,"2020-01-19");
+Insert into health(usersID,water_units,alchool_units,steps,calories,input_date) 
+VALUES ("2323e2wdwqdw123",8,0,1000,3000,"2020-01-19");
+Select * from health;
+/*Select * from Stresses;
+Select * from health*/
