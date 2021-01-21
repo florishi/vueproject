75c83c85d282e24618038e7f8dddb14a6ddd45d3
florishi <flroisihi12@gmail.com>
2021-01-21 00:29:41 +0000

    new table

--- db/schema.sql
+++ db/schema.sql
@@ -4,15 +4,15 @@ CREATE DATABASE sanaya;
 USE sanaya;
 
 create table Users(
-    id integer not null unique auto_increment,
-	sessionId varchar(22)  not null unique,
+	id integer not null unique auto_increment,
+	sessionId varchar(22) not null,
     Email varchar(50),
     userName varchar(50)
 );
 
 create table Stresses(
 	id integer not null unique auto_increment,
-    usersID varchar(22),
+    usersId integer,
     moods varchar(10),
     hoursSleep integer,
     minsExercise integer,
@@ -22,10 +22,17 @@ create table Stresses(
 
 create table health(
 	id integer not null unique auto_increment,
-    usersID varchar(22),
+    usersId integer,
 	waterUnits integer,
     alchoolUnits integer,
     steps integer,
     calories integer,
     inputDate date
 );
+
+create table validators(
+	id  integer not null unique auto_increment,
+    usersId integer,
+    valueKey varchar(22)
+);
+
