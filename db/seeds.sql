2fb39a3f77b4e58cb1b586586cfa2e66925f5ecc
florishi <flroisihi12@gmail.com>
2021-02-02 20:43:42 +0000

    commit to pull

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,5 @@
-Insert into Users(id,Email,userName) VALUES ("2323e2wdwqdw123","g@gmail.com","Guga");
+Insert into Users(sessionId,Email,userName) VALUES ("QCuma5iXpF_rRgan_EY3p","sandy@test.com","Sandy");
 Insert into Stresses(usersID,moods,hoursSleep,minsExercise,coffeeUnits,inputDate) 
-VALUES ("2323e2wdwqdw123","happy",8,30,0,"2020-01-19");
+Values (1,"happy",8,30,0,"2020-01-20");
 Insert into health(usersID,waterUnits,alchoolUnits,steps,calories,inputDate) 
-VALUES ("2323e2wdwqdw123",8,0,1000,3000,"2020-01-19");
-Select * from health;
-/*Select * from Stresses;
-Select * from health*/
+VALUES (1,1,0,1000,3000,"2020-01-20");
