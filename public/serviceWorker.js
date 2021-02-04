1a513e0d0631b6f74d071a64e3e438c4533f5a9f
florishi <flroisihi12@gmail.com>
2021-02-04 20:04:16 +0000

    service worker file

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
 const FILES_TO_CACHE = [
-  "/img/alcohol-01.png",
-  "/"
+  '/img/alcohol-01.png',
+  '/'
 ];
 // install
-self.addEventListener("install", function (evt) {
+self.addEventListener('install', function (evt) {
   // pre cache image data
   evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/img/alcohol-01.png"))
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.add('/img/alcohol-01.png'))
   );
   // pre cache all static assets
   evt.waitUntil(
@@ -19,13 +19,13 @@ self.addEventListener("install", function (evt) {
   self.skipWaiting();
 });
 // activate
-self.addEventListener("activate", function(evt) {
+self.addEventListener('activate', function(evt) {
   evt.waitUntil(
     caches.keys().then(keyList => {
       return Promise.all(
         keyList.map(key => {
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
+            console.log('Removing old cache data', key);
             return caches.delete(key);
           }
         })
@@ -35,8 +35,8 @@ self.addEventListener("activate", function(evt) {
   self.clients.claim();
 });
 // fetch
-self.addEventListener("fetch", function(evt) {
-  if (evt.request.url.includes("/api/")) {
+self.addEventListener('fetch', function(evt) {
+  if (evt.request.url.includes('/api/')) {
     evt.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(evt.request)
@@ -48,7 +48,10 @@ self.addEventListener("fetch", function(evt) {
             return response;
           })
           .catch(err => {
-            // Network request failed, try to get it from the cache.
+            // Network request failed, try to get it from the cache
+            if(err){
+              console.log(err);
+            }
             return cache.match(evt.request);
           });
       }).catch(err => console.log(err))
@@ -64,10 +67,10 @@ self.addEventListener("fetch", function(evt) {
   );
 });
 
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      navigator.serviceWorker.register("serviceWorker.js").then(reg => {
-        console.log("Our service worker file is installed", reg);
-      });
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('serviceWorker.js').then(reg => {
+      console.log('Our service worker file is installed', reg);
     });
-  }
+  });
+}
